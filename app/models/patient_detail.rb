908afe32cd23405e55bcade7fc1a77b51c93eb87
Manisha Panda <mpanda27@tamu.edu>
2023-10-20 23:31:13 -0500

    Added Extra test Cases and Made PID Unique

--- app/models/patient_detail.rb
+++ app/models/patient_detail.rb
@@ -1,3 +1,4 @@
 class PatientDetail < ApplicationRecord
     validates :PatientId, uniqueness: { scope: :SchoolName, message: 'Duplicate Patient details for the same SchoolName' }
+    validates :PID, uniqueness: true
 end
--- spec/views/patientDetails/index.html.erb_spec.rb
+++ spec/views/patientDetails/index.html.erb_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "patient_details/index", type: :view do
         Age: 2,
         Insurance: "Insurance",
         Grade: "Grade",
-        PID: "PID", 
+        PID: "1-School Name", 
         PatientId: 1
     ),
     PatientDetail.create!(
@@ -18,7 +18,7 @@ RSpec.describe "patient_details/index", type: :view do
       Age: 2,
       Insurance: "Insurance",
       Grade: "Grade",
-      PID: "PID", 
+      PID: "2-School Name", 
       PatientId: 2
   )
     ])
@@ -29,5 +29,33 @@ RSpec.describe "patient_details/index", type: :view do
     # cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
     assert_select 'tr>td', text: Regexp.new("Program Name".to_s), count: 2
     assert_select 'tr>td', text: Regexp.new("School Name".to_s), count: 2
+  end
+
+  it "validates uniqueness of PID" do
+    patient_detail = PatientDetail.new(
+      ProgramName: "Program Name",
+      SchoolName: "School Name",
+      Age: 2,
+      Insurance: "Insurance",
+      Grade: "Grade",
+      PID: "1-School Name",  # Duplicate PID
+      PatientId: 3
+    )
+
+    expect(patient_detail).not_to be_valid
+  end
+
+  it "validates uniqueness of PatientId within the same SchoolName" do
+    patient_detail = PatientDetail.new(
+      ProgramName: "Program Name",
+      SchoolName: "School Name",
+      Age: 2,
+      Insurance: "Insurance",
+      Grade: "Grade",
+      PID: "3-School Name",
+      PatientId: 1  # Duplicate PatientId within the same SchoolName
+    )
+
+    expect(patient_detail).not_to be_valid
   end
 end
