8afb428716e1870d4969e806d991ba1d4d1c9529
Manisha Panda <mpanda27@tamu.edu>
2023-10-23 11:03:11 -0500

    Added Required Fields

--- app/models/patient_detail.rb
+++ app/models/patient_detail.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class PatientDetail < ApplicationRecord
+  validates :PatientId, presence: true
+  validates :SchoolName, presence: true
+  validates :Age, presence: true
+  validates :Date, presence: true
+  validates :Insurance, presence: true
+  validates :Grade, presence: true
   validates :PatientId,
             uniqueness: { scope: :SchoolName, message: 'Duplicate Patient details for the same SchoolName' }
   validates :PID, uniqueness: true
