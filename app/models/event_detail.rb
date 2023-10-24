18aa4bf53bb9eae1c3e97ba2244a902dfe3e68ca
apurvamandalika-tamu <apurva.mandalika@tamu.edu>
2023-10-24 12:45:34 -0500

    Modified event_details views and added rspec tests

--- app/models/event_detail.rb
+++ app/models/event_detail.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 class EventDetail < ApplicationRecord
+  validates :EventDate, presence: true
+  validates :School, presence: true
+  validates :ConsentFD, presence: true
+  validates :DenHrs, presence: true
+  validates :DenTravelHrs, presence: true
+  validates :DenTravelMil, presence: true
+  validates :HygHours, presence: true
+  validates :HygTravelMil, presence: true
+  validates :HygTravelHrs, presence: true
+  validates :AssistantTravelMil, presence: true
+  validates :AssistantHrs, presence: true
+  validates :AssistantTravelHrs, presence: true
+  validates :OtherHrs, presence: true
+  validates :OtherTravelHrs, presence: true
+  validates :OtherTravelMiles, presence: true
+  validates :NumberOfSSPDriven, presence: true
+  validates :TotalMilesDriven, presence: true
+  # Not needed in event details page
+  validates :ChildScreened, presence: false, allow_blank: true
+  validates :ChildReceivingSealant, presence: false, allow_blank: true
+  validates :NumberOfSealed, presence: false, allow_blank: true
+  validates :NumberFlourideVarnish, presence: false, allow_blank: true
+  validates :NumberProphy, presence: false, allow_blank: true
 end
