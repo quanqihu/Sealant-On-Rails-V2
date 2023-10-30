83b4fc2270511a54a800c9f537ccb3139dcce1e6
Jaydeep Radadiya <jdr@tamu.edu>
2023-10-30 10:51:40 -0500

    Updated Export Logic
    - Right now there is a placeholder solution to exporting patient details.
    - Will improve this to export child level details also with it.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   get '/export_event_details', to: 'excel_exports#export_event_details'
-  get '/export_child_level_details_with_patient_info', to: 'excel_exports#export_child_level_details'
+  get '/export_child_level_details_with_patient_info', to: 'excel_exports#export_patient_details'
   resources :child_level_details
   get 'statistics/index'
   get 'statistics/school'
