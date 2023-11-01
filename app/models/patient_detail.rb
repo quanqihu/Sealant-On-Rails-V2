33745495b2d1c3ee2d7acd72c28161e445accecb
Jaydeep Radadiya <jdr@tamu.edu>
2023-11-01 00:46:41 -0500

    Updated Child Level View
    - Added Back Button
    - Made the selection for the screening, preventative and follow up buttons.
    - Added more statistics option.
    - Next will improve the showing and the form

--- app/models/patient_detail.rb
+++ app/models/patient_detail.rb
@@ -10,6 +10,5 @@ class PatientDetail < ApplicationRecord
   validates :PatientId,
             uniqueness: { scope: :SchoolName, message: 'Duplicate Patient details for the same SchoolName' }
   validates :PID, uniqueness: true
-  has_many :tooths
   has_many :child_level_details, foreign_key: 'PID', primary_key: 'PID'
 end
