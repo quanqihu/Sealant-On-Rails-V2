9cea6ceb8dfeb9ae95e8dd85dbb6aae6ead9532e
SushantTamu <sushant.shelar@tamu.edu>
2023-11-10 15:00:04 -0600

    added logic for screening data to save and show default values

--- app/javascript/formSubmission.js
+++ app/javascript/formSubmission.js
@@ -2,24 +2,33 @@ document.addEventListener("submit", function(event) {
     event.preventDefault();
     // console.log("Form Submitted");
 
+
+    var popupScreening = {}; // Initialize an empty dictionary
+
+    function handlePopupSelection(toothNumber, selectedValue) {
+
+        popupScreening[toothNumber.toString()] = selectedValue;
+    //   console.log(popupData); // Optional: Log the dictionary to the console
+    }
+
+
+    for (var toothNumber = 1; toothNumber <= 32; toothNumber++) {
+        var button = document.getElementById('ScreeningButton' + toothNumber);
+        var selectedValue = button.innerText.trim();
+
+        if (selectedValue == "Make a Selection")
+        {
+            selectedValue=""
+        }
+        handlePopupSelection(toothNumber.toString(), selectedValue);
+      }
+
+
     if (event.submitter && event.submitter.id === "submitButton") {
         var form = event.target;
 
-        // var childLevelDetailID = new URLSearchParams(window.location.search).get('patient_detail_id');
-        // // var pid = form.querySelector('[name="child_level_detail[PID]"]').value;
-
-        // if (childLevelDetailID) {
-        //     // If an ID exists, this is an update
-        //     form.action = '/child_level_details/' + childLevelDetailID;
-        //     form.method = 'patch';
-        // } else {
-        //     // If there's no ID, it's a new record creation
-        //     form.action = '/patient_details/' + pid + '/child_level_details';
-        //     form.method = 'post';
-        // }
-
         // Set the value of TeethScreening using your existing logic
-        var TeethScreeningStr = JSON.stringify(selectedOptions);
+        var TeethScreeningStr = JSON.stringify(popupScreening);
         var TeethScreeningInput = form.querySelector('[name="child_level_detail[TeethScreening]"]');
         TeethScreeningInput.value = TeethScreeningStr;
 
